af3c5cb4598348fd8f8229226dad73185155ca3b
piwer-m <volna.cmi.oovc@gmail.com>
2022-11-18 21:43:51 +0300

    Menu, footer
    add menu
    add footer

---
+++ assets/js/base.js
@@ -0,0 +1,40 @@
+let en = {
+    "menu": `<menu>
+        <div class="menu-element"><a href="/ru">Main</a></div>
+        <div class="menu-element"><a href="/ru/news">News</a></div>
+        <div class="menu-icon"><a href="/ru"><img src="../assets/imgs/favicon.ico" class="icon"></a></div>
+        <div class="menu-element"><a href="/ru/country">Countries</a></div>
+        <div class="menu-element"><a href="/ru/about">About</a></div>
+    </menu>`,
+    "footer": `<footer>
+        <div class="links">
+            <div class="vk"><a href="vk.com/volna_cmi">VK</a></div>
+            <div class="about"><a href="/ru/about">About</a></div>
+        </div>
+        <div class="icon"><img src="../assets/imgs/favicon.ico" alt="icon"></div> 
+    </footer>`
+}
+
+let ru = {
+    "menu": `<menu>
+        <div class="menu-element"><a href="/ru">Главная</a></div>
+        <div class="menu-element"><a href="/ru/news">Новости</a></div>
+        <div class="menu-icon"><a href="/ru"><img src="../assets/imgs/favicon.ico" class="icon"></a></div>
+        <div class="menu-element"><a href="/ru/country">Страны</a></div>
+        <div class="menu-element"><a href="/ru/about">О Волне</a></div>
+    </menu>`,
+    "footer": `<footer>
+        <div class="links">
+            <div class="vk"><a href="vk.com/volna_cmi">VK</a></div>
+            <div class="about"><a href="/ru/about">О Волне</a></div>
+        </div>
+        <div class="icon"><img src="../assets/imgs/favicon.ico" alt="icon"></div> 
+    </footer>`
+}
+
+window.onload = function() {
+    let menu = navigator.language == "ru" ? ru["menu"] : en["menu"];
+    let footer = navigator.language == "ru" ? ru["footer"] : en["footer"];
+
+    document.querySelector("body").innerHTML = menu + document.querySelector("body").innerHTML + footer;
+};
